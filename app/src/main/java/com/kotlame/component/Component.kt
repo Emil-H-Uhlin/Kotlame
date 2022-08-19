1e624b3cd437869042e463ca3dd79784b3efbc36
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-08-19 21:53:16 +0200

    Feat: added component WIP

---
+++ app/src/main/java/com/kotlame/component/Component.kt
@@ -0,0 +1,13 @@
+package com.kotlame.component
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import com.kotlame.gameobjects.GameObject
+
+abstract class Component {
+    lateinit var gameObject: GameObject
+
+    open fun initialize(): Unit? = null
+    open fun update(): Unit? = null
+    open fun draw(canvas: Canvas, paint: Paint?): Unit? = null
+}
