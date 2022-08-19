27c1c3bf2edf427b7bee282d95d2888788f649f6
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-08-19 19:34:23 +0200

    Feat: coroutine game loop base

--- app/src/main/java/com/kotlame/game/Game.kt
+++ app/src/main/java/com/kotlame/game/Game.kt
@@ -2,15 +2,40 @@ package com.kotlame.game
 
 import android.content.Context
 import android.view.SurfaceView
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class Game(ctx: Context): SurfaceView(ctx) {
     @Volatile var running = false
 
-    fun resume() {
+    fun resume() = MainScope().launch {
         running = true
+
+        while (running) {
+            update()
+            draw()
+        }
     }
 
     fun pause() {
         running = false
+    }
+
+    private fun update() {
+        
+    }
+
+    private fun draw() {
+        if (!holder.surface.isValid) return
+
+        val canvas = holder.lockCanvas()
+
+        /* Draw your game objects */
+
+        canvas.restore()
+
+        /* Draw UI */
+
+        holder.unlockCanvasAndPost(canvas)
     }
 }
