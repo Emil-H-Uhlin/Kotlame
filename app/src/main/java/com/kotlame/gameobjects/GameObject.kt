456b58102a8f2791af5a4b04e0fe6eecae63b091
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-08-19 22:02:11 +0200

    Feat: add component to game object and get component of type from game object

--- app/src/main/java/com/kotlame/gameobjects/GameObject.kt
+++ app/src/main/java/com/kotlame/gameobjects/GameObject.kt
@@ -11,20 +11,26 @@ class GameObject private constructor() {
         fun withComponent(component: Component) = apply {
             gameObject.addComponent(component)
         }
+
+        fun build() = gameObject
     }
 
-    private val components = arrayListOf<Component>()
-
+    val components = arrayListOf<Component>()
     var destroyed = false
 
     fun addComponent(component: Component): Boolean {
         if (components.add(component)) {
+            component.gameObject = this
+            component.initialize()
 
+            return true
         }
 
         return false
     }
 
-    fun update(deltaTime: Double) = components.forEach { }
-    fun draw(canvas: Canvas, paint: Paint?) = components.forEach { }
+    fun update(deltaTime: Double) = components.forEach { it.update(deltaTime) }
+    fun draw(canvas: Canvas, paint: Paint?) = components.forEach { it.draw(canvas, paint) }
+
+    inline fun <reified componentType: Component> getComponent(): componentType? = components.find { component -> component is componentType } as componentType?
 }
